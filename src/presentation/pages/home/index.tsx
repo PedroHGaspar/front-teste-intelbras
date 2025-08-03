eb6ee06fd9127ba9aa693ba837d8a92923a808d7
PedroHGaspar <ordep12@hotmail.com.br>
2025-08-03 13:36:42 -0300

    Feature: breve disclaimer sobre o projeto

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -6,8 +6,17 @@ export const HomePage = () => {
   return (
     <div className={styles.containerPage}>
       <Title.Root size="medium">
-        {/* <Title.Text>HOME</Title.Text> */}
+        <Title.Text>Bem-vindos 👋</Title.Text>
       </Title.Root>
+
+      <p className={styles.paragrafo_home}>
+        Obrigado pela oportunidade de participar deste desafio técnico!<br /><br />
+        Eu separei meu desenvolvimento em 4 etapas: funcionalidades obrigatórias, limpeza de código, estilização e funcionalidades extras.<br /><br />
+        O maior desafio foi na lógica de reversão de exclusão com múltiplos cards isolados, foi o que eu levei mais tempo pra fazer por conta de achar a maneira certa de isolar os cards pra que cada um se comportasse de forma única.
+        <br /><br />
+        Posso esclarecer sobre qualquer ponto do projeto, explicar minhas decisões ou melhorias futuras.
+      </p>
     </div>
   );
 };
+
--- src/presentation/pages/home/styles/home-page.css.ts
+++ src/presentation/pages/home/styles/home-page.css.ts
@@ -2,7 +2,14 @@ import { style } from "@vanilla-extract/css";
 
 
 export const containerPage = style({
-    marginTop: "20rem",
+    marginTop: "5rem",
     marginLeft: "5%",
     height: "90vh",
+});
+
+export const paragrafo_home = style({
+    marginTop: "1rem",
+    fontSize: "14px",
+    maxWidth: "620px",
+    lineHeight: 1.6
 });
